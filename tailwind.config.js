d897aca048a056d6798c53c5f519ab08e2f173e9
snels0n <sam.nelsung12@gmail.com>
2024-11-05 00:00:12 -0500

    "initial commit"

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary': "##d6dbdf",
+        'secondary': "#2e4053"
+      },
+    },
+  },
+  plugins: [],
+}
+
